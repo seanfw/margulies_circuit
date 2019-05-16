7536947f76541d99336cc68b3056eba3fc71450d
margulies <daniel.margulies@gmail.com>
2019-05-16 08:45:09 +0200

    general speed ups based on line profiling

--- model/run.py
+++ model/run.py
@@ -5,10 +5,10 @@ from local_circuit import initialize_local, current_to_frequency, NMDA_deriv, GA
 from model import get_stim
 from delays import get_delays
 from initialize_parameters import initialize_pm
-from numba_code import dot_np, matmul
 import brian2
 import numpy as np
-from numba import jit
+from numba import jit, njit, prange
+import numpy.matlib
 import matplotlib.pyplot as plt
 import nibabel as nib
 import nilearn
@@ -46,11 +46,19 @@ V1_index       = get_stim(c.get('annot_file'), c.get('label_name'), c.get('cort_
 I_ext          = np.zeros((num_iterations,num_nodes,pm['num_pops'])) * brian2.amp
 I_ext[int(stim_on/dt):int(stim_off/dt),V1_index,0] = pl['stim_strength']
 
+@njit(parallel=True)
+def mk_tr_delay(tr, sr, pr):
+    for idx in prange(tr.shape[0]):
+        for idy in prange(tr.shape[0]):
+            sr[idx,idy] = pr[tr[idx,idy],idy]
+    return sr
+
 # run model
-for i_t in range(1,num_iterations):
+#@profile
+def run_model(i_t, pm, pl, S_tract_delay, tract_connectivity, surface_connectivity, tract_delays, I_ext, dt, num_nodes):
 
     # update noise - dims = num local pops x num areas
-    pm['I_noise'] = pm['I_noise'] + -pm['I_noise']*(dt/pl['tau_AMPA']) + pm['noise_rhs'][i_t-1,:,:]
+    pm['I_noise'] = pm['I_noise'] + -pm['I_noise'] * (dt/pl['tau_AMPA']) + pm['noise_rhs'][i_t-1,:,:]
 
     # local NMDA
     pm['I_local_NMDA'][i_t-1,:,:] = pm['J_NMDA'].dot(pm['S_NMDA'][i_t-1,:,:].T).T
@@ -60,13 +68,13 @@ for i_t in range(1,num_iterations):
 
     # long range NMDA (along white matter tracts) with variable delays
     if c.get('long_range') == 'True':
-        conn_delay = np.zeros(shape=(np.shape(tract_connectivity)))
-        conn_delay = matmul(tract_connectivity,S_tract_delay,conn_delay)
-        pm['I_longRange_NMDA'][i_t-1,:,0] = pl['g_E_longRange'] * np.sum(conn_delay,axis=1)
+        d_tract = np.einsum('ij,ij->i',tract_connectivity,S_tract_delay)
+        pm['I_longRange_NMDA'][i_t-1,:,0] = pl['g_E_longRange'] * d_tract
+        #pm['I_longRange_NMDA'][i_t-1,:,0] = pl['g_E_longRange'] * np.sum(np.multiply,tract_connectivity,S_tract_delay),axis=1)
 
     # mid range NMDA (along surface), with a 7 timestep delay
     if c.get('mid_range') == 'True':
-        pm['I_midRange_NMDA'][i_t-1,:,:] = pl['g_E_midRange'] * dot_np(surface_connectivity,pm['S_NMDA'][i_t-pl['midrange_delay'],:,:])
+        pm['I_midRange_NMDA'][i_t-1,:,:] = pl['g_E_midRange'] * surface_connectivity.dot(pm['S_NMDA'][i_t-pl['midrange_delay'],:,:])
 
     # Define total input current as sum of local NMDA & GABA inputs, with background, external currents & noise & longRange
     if c.get('long_range')  == 'True' and c.get('mid_range') == 'True':
@@ -79,19 +87,26 @@ for i_t in range(1,num_iterations):
         pm['I_total'][i_t-1,:,:] = pm['I_local_NMDA'][i_t-1,:,:] +  pm['I_local_GABA'][i_t-1,:,:] + pm['I_0'] + I_ext[i_t-1,:,:] + pm['I_noise']
 
     # Update the firing rates of the excitatory population
-    pm['R'][i_t,:,0] = pm['R'][i_t-1,:,0] + dt*current_to_frequency(pm['I_total'][i_t-1,:,0],'E',pl)/pl['tau_rates']-dt*pm['R'][i_t-1,:,0]/pl['tau_rates']
+    pm['R'][i_t,:,0] = pm['R'][i_t-1,:,0] + dt * current_to_frequency(pm['I_total'][i_t-1,:,0],'E',pl) / pl['tau_rates'] - dt * pm['R'][i_t-1,:,0] / pl['tau_rates']
 
     # Update the firing rates of the inhibitory population.
-    pm['R'][i_t,:,1] = pm['R'][i_t-1,:,1] + dt*current_to_frequency(pm['I_total'][i_t-1,:,1],'I',pl)/pl['tau_rates']-dt*pm['R'][i_t-1,:,1]/pl['tau_rates']
+    pm['R'][i_t,:,1] = pm['R'][i_t-1,:,1] + dt * current_to_frequency(pm['I_total'][i_t-1,:,1],'I',pl) / pl['tau_rates']-dt * pm['R'][i_t-1,:,1] / pl['tau_rates']
 
     # Update the NMDA synapses
-    pm['S_NMDA'][i_t,:,0] = pm['S_NMDA'][i_t-1,:,0] + dt*NMDA_deriv(pm['S_NMDA'][i_t-1,:,0],pm['R'][i_t,:,0],pl)
+    pm['S_NMDA'][i_t,:,0] = pm['S_NMDA'][i_t-1,:,0] + dt * NMDA_deriv(pm['S_NMDA'][i_t-1,:,0],pm['R'][i_t,:,0],pl)
 
     if c.get('long_range') == 'True':
-        S_tract_delay = pm['S_NMDA'][i_t-tract_delays,range(num_nodes),0]
+        snmda = np.asarray(pm['S_NMDA'][:,:,0]).squeeze()
+        S_tract_delay = mk_tr_delay(tract_delays-i_t, S_tract_delay, snmda)
+        #S_tract_delay = pm['S_NMDA'][i_t-tract_delays,range(num_nodes),0]
 
     # Update the GABA synapses
-    pm['S_GABA'][i_t,:,1] = pm['S_GABA'][i_t-1,:,1] + dt*GABA_deriv(pm['S_GABA'][i_t-1,:,1],pm['R'][i_t,:,1],pl)
+    pm['S_GABA'][i_t,:,1] = pm['S_GABA'][i_t-1,:,1] + dt * GABA_deriv(pm['S_GABA'][i_t-1,:,1],pm['R'][i_t,:,1],pl)
+
+    return pm, pl, S_tract_delay
+
+for i_t1 in range(1,num_iterations):
+    pm, pl, S_tract_delay = run_model(i_t1, pm, pl, S_tract_delay, tract_connectivity, surface_connectivity, tract_delays, I_ext, dt, num_nodes)
 
 # save output
 if c.get('save_output') == 'True':
