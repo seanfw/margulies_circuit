733ab6eac2c0b050cd8eecde2c395e9a410d34ce
margulies <daniel.margulies@gmail.com>
2019-05-16 16:35:37 +0200

    fixing install

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ def load_requirements(fname):
 
 setup(
     name='circuit',
-    version='0.0.1',
+    version='0.0.2',
     packages=['circuit'],
-    url='http://pypi.python.org/pypi/PackageName/',
     license='LICENSE.txt',
     description='circuit model',
     long_description=open('README.md').read(),
